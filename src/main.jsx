86fcb7237975dfaf0899c957bb555c4daf926c84
Ruturaj Nikam <ruturajnikam1010@gmail.com>
2024-11-06 23:59:14 +0530

    bug fix for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './store/Store.jsx'
+import store from './store/Store.jsx'
 
 createRoot( document.getElementById( 'root' ) ).render(
   <Provider store={ store }>
