182e47f3c5191c5f4be783d29bf5fc3aab5447ff
Ruturaj <ruturajnikam1010@gmail.com>
2024-12-03 00:33:27 +0530

    handled future version error

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store/Store.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/Store.jsx";
 
-createRoot( document.getElementById( 'root' ) ).render(
-  <Provider store={ store }>
-    <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <App />
     </BrowserRouter>
   </Provider>
-
-)
+);
